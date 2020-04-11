5e4d67840a1e3a9cef35f86d0e65f1d06800f734
Christian99 <cristhian_99_piscis@hotmail.com>
2020-04-11 02:45:55 -0500

    changes
    Signed-off-by: Christian99 <cristhian_99_piscis@hotmail.com>

---
+++ Electron-images/src/public/js/profile/profile.js
@@ -0,0 +1,56 @@
+$(function() {
+    $('#My-profile').click(function(e) {
+        e.preventDefault();
+        $('.my-profile').removeAttr('hidden');
+        $('.edit-profile').attr('hidden', 'true');
+    });
+    $('#Edit-profile').click(function(e) {
+        e.preventDefault();
+        $('.edit-profile').removeAttr('hidden');
+        $('.my-profile').attr('hidden', 'true');
+    });
+
+    $('#confirm-password').keyup(function(e) {
+        const password = $('#password').val();
+        if (e.target.value == password) {
+            $('#confirm-password').removeClass('btn btn-outline-danger');
+            $('#confirm-password').addClass('btn btn-outline-success');
+            $('.btn-confirm').removeAttr('disabled');
+        } else {
+            $('#confirm-password').removeClass('btn btn-outline-success');
+            $('#confirm-password').addClass('btn btn-outline-danger');
+            $('.btn-confirm').attr('disabled', 'true');
+        }
+    });
+
+    $('.btn-confirm').click(function(e) {
+        e.preventDefault();
+        swal('Esta seguro de editar ?', {
+            icon: 'warning',
+            buttons: ['Cancelar', 'Actualizar'],
+            closeOnEsc: false,
+            closeOnClickOutside: false,
+            dangerMode: true
+        }).then(async(result) => {
+            if (result) {
+                await swal('Se edito correctamente', {
+                    timer: 2000,
+                    icon: 'success',
+                    closeOnEsc: false,
+                    closeOnClickOutside: false,
+                    button: false
+                });
+
+            } else {
+                await swal('Edicion cancelada', {
+                    timer: 2000,
+                    icon: 'error',
+                    closeOnEsc: false,
+                    closeOnClickOutside: false,
+                    button: false
+                });
+            }
+            $('.btn-secondary').trigger('click');
+        });
+    });
+});
